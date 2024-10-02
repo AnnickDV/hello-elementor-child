33dc129817a0a94869362deba18273efe17fadfb
AnnickDV <annick.vivicorsi@gmail.com>
2024-10-02 12:04:14 +0200

    V3

--- functions.php
+++ functions.php
@@ -5,4 +5,14 @@ function enqueue_parent_styles() {
    wp_enqueue_style( 'enfant-style', get_stylesheet_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+function add_extra_item_to_nav_menu( $items, $args ) {
+    if (is_user_logged_in() && $args->menu == 303) {
+        $items .= '<li><a href="'. get_permalink( get_option('woocommerce_myaccount_page_id') ) .'">Admin</a></li>';
+    }
+    elseif (!is_user_logged_in() && $args->menu == 303) {
+        $items .= '<li><a href="' . get_permalink( wc_get_page_id( 'myaccount' ) ) . '">Sign in  /  Register</a></li>';
+    }
+    return $items;
+}
 ?>
