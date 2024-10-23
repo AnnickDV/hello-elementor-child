43f97081d646ca5ce64fbb09edbb2994b8ce00c6
AnnickDV <annick.vivicorsi@gmail.com>
2024-10-23 06:52:47 +0200

    V14 DESK

--- functions.php
+++ functions.php
@@ -9,7 +9,10 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
 function add_extra_item_to_nav_menu( $items, $args ) {
     if (is_user_logged_in()) {
-        $items .= '<li class="menu-item menu-item-type-post_type menu-item-object-page parent hfe-creative-menu"><a class="hfe-menu-item" href="'. get_admin_url().'"><div class="admin">Admin</div></a></li>';
+        $items .= '<li class="menu-item menu-item-type-post_type menu-item-object-page 
+        parent hfe-creative-menu">
+        <a class="hfe-menu-item" href="'. get_admin_url().'">
+        <div class="admin">Admin</div></a></li>';
     }
     return $items;
 }
